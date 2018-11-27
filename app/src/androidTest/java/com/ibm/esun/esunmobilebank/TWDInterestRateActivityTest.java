f6a6fb2c7781c119e4d95e92a71406fd320c694b
r88881253 <r88881253@yahoo.com.tw>
2018-11-27 13:30:18 +0800

    [feature] add screenshot

--- app/src/androidTest/java/com/ibm/esun/esunmobilebank/TWDInterestRateActivityTest.java
+++ app/src/androidTest/java/com/ibm/esun/esunmobilebank/TWDInterestRateActivityTest.java
@@ -3,9 +3,14 @@ package com.ibm.esun.esunmobilebank;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -15,13 +20,15 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class TWDInterestRateActivityTest {
 
-
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
     @Rule
     public final ActivityTestRule<TWDInterestRateActivity> main = new ActivityTestRule<>(TWDInterestRateActivity.class);
 
     @Test
     public void screenOutput() throws InterruptedException {
-
+        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
+        Screengrab.screenshot("before_button_click");
 //        onView(withId(R.id.toolbar)).check(matches(withText("hello")));
         Thread.sleep(2000);
 
